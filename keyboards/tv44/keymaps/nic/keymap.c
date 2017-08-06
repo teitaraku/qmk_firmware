8da562e90369dbdeb1911e73fa07ca35ff42cfa7
Nic Aitch <nic@nicinabox.com>
2017-08-06 16:00:47 -0500

    Update minivan

--- keyboards/tv44/keymaps/nic/keymap.c
+++ keyboards/tv44/keymaps/nic/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWERTY] = {
     {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
     {CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
-    {TD(SFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, TG(NORMAN) },
+    {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_CAPS },
     {MO(LOWER), KC_LALT, KC_LGUI, F(1),  XXXXXXX, XXXXXXX, XXXXXXX, F(2),  KC_RGUI, KC_LEFT,  KC_DOWN,  KC_RIGHT }
   },
   [LOWER] = {
